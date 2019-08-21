f2f34a454d5b6be1330b04a4e34e131c8fa3eb51
孟庆猛 <762577099@qq.com>
2019-08-21 20:41:37 +0800

    客户端与服务端通信

--- src/main/java/top/mengtech/server/Server.java
+++ src/main/java/top/mengtech/server/Server.java
@@ -1,8 +1,12 @@
 package top.mengtech.server;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 public class Server {
     private static final int PORT = 8000;
@@ -13,6 +17,26 @@ public class Server {
 
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.group(bossGroup,workerGroup)
-                .channel(NioServerSocketChannel.class);
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new ServerHandler());
+                    }
+                });
+
+        bind(serverBootstrap,PORT);
+    }
+
+    private static void bind(final ServerBootstrap serverBootStrap,final int port){
+        serverBootStrap.bind(8000).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("端口["+ port +"]绑定成功");
+                }else{
+                    System.out.println("端口["+ port +"]绑定失败");
+                    bind(serverBootStrap,port + 1);
+                }
+            }
+        });
     }
 }
