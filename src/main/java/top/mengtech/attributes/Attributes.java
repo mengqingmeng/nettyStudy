5b94c01db7df2bbb768ef844824b6ef23762b345
孟庆猛 <762577099@qq.com>
2020-03-12 20:10:51 +0800

    init BIO branch

--- src/main/java/top/mengtech/attributes/Attributes.java
+++
@@ -1,8 +0,0 @@
-package top.mengtech.attributes;
-
-import io.netty.util.AttributeKey;
-
-public interface Attributes {
-    // 登陆成功标志位
-    AttributeKey<Boolean> LOGIN = AttributeKey.newInstance("login");
-}
--- src/main/java/top/mengtech/serializer/SerializerAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package top.mengtech.serializer;
-
-/**
- * 序列化算法类型
- */
-public interface SerializerAlgorithm {
-    byte JSON = 1;
-}
